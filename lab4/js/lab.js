268d126b4a07ad4f981c9150d0f89102c0ea58e1
helloimjennie <jenniferle@mail.com>
2024-04-23 22:33:58 -0700

    Worked on Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - An implmention of the game Rock-Paper-Scissors
+// Author: Jennifer Le and Gabriela Botto 
+// Date: 18 April 2024 
+
+/* 
+
+Task 2 Outline: Rock-Paper-Scissors 
+1. Players face each other and usually count to three aloud. 
+Common chants include "Rock, Paper, Scissors" or "One, Two, Three, Shoot"
+2. Determining the Winner: 
+The winner is detemined based on the rules of which gestures defeat the others:
+Rock crushes Scissors (Rock wins), Scissors cut Paper (Scissors win), 
+Paper covers Rock (Paper wins)
+If both players choose the same gesture, the game is a tie, and no one wins that 
+round. Players typically play around round to break the tie.
+3. Gameplay Duration: 
+Before starting, players can agree on the number of rounds to play, such as 
+best of three or best of five. The player who wins the most rounds is the overall
+winner of the game. 
+4. Fair Play: 
+Players must form their gestures at the exact same time to ensure fairness. Delayed
+reveals or changing gestures mid-reveal are considered cheating. 
+
+*/
 
 // Constants
 
