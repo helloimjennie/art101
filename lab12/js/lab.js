43ae62972eae6864034c75d8f0ed391ee2747847
helloimjennie <jenniferle@mail.com>
2024-05-24 01:44:04 -0700

    Lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,24 +1,29 @@
-js 
-x=121;
+$(document).ready(function() {
+    // Event listener for the button click
+    $("#button").click(function() {
+        var name = $("#input").val().trim();
+        if (name === "") {
+            alert("Please enter a name.");
+            return;
+        }
 
-function sortingHat(str){
-    len = str.length;
-    mod = len % 4;
-    if(mod == 0) {
-      return "snarflepuff";
-    } else if (mod == 1){
-      return "Glynethpaltrow";
-    } else if (mod == 2) {
-      return "SltheringSnake";
-    } else if (mod == 3) {
-      return "Paramour";
-    } 
-  }
-  
-  var myButton = document.getElementById("button");
-  myButton.addEventListener("click", function() {
-    var name = document.getElementById("input").value;
-    var house = sortingHat(name);
-    newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
-    document.getElementById("output").innerHTML = newText;
-  })
+        // Sorting hat function
+        function sortingHat(str) {
+            var len = str.length;
+            var mod = len % 4;
+            if (mod === 0) {
+                return "Snarflepuff";
+            } else if (mod === 1) {
+                return "Glynethpaltrow";
+            } else if (mod === 2) {
+                return "SlytheringSnake";
+            } else if (mod === 3) {
+                return "Paramour";
+            }
+        }
+
+        var house = sortingHat(name);
+        var newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+        $("#output").html(newText);
+    });
+});
